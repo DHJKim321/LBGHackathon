7338efdcc32dda2c814821ea5b0f90ff2973469d
Daniel Kim <dhjkim2001@gmail.com>
2022-07-28 22:07:31 +0900

    Added check boxes

--- LBGHackathon.py
+++ LBGHackathon.py
@@ -1,6 +1,31 @@
 import tkinter as tk
 import time
 
+class ChecklistBox(tk.Frame):
+    def __init__(self, parent, choices, **kwargs):
+        tk.Frame.__init__(self, parent, **kwargs)
+
+        self.vars = []
+        bg = self.cget("background")
+        for choice in choices:
+            var = tk.StringVar(value=choice)
+            self.vars.append(var)
+            cb = tk.Checkbutton(self, var=var, text=choice,
+                                onvalue=choice, offvalue="",
+                                anchor="w", width=20, background=bg,
+                                relief="flat", highlightthickness=0
+            )
+            cb.pack(side="top", fill="x", anchor="w")
+
+
+    def getCheckedItems(self):
+        values = []
+        for var in self.vars:
+            value =  var.get()
+            if value:
+                values.append(value)
+        return values
+
 class Pomodoro(tk.Frame):
 
     def countdownTimer(self):
@@ -14,7 +39,7 @@ class Pomodoro(tk.Frame):
             #Update the time
             self.update()
             time.sleep(1)
-            if(times == 0):
+            if (times == 0):
                 self.isBreak = not self.isBreak
                 if not self.isBreak:
                     self.sec.set('00')
@@ -29,9 +54,14 @@ class Pomodoro(tk.Frame):
         tk.Frame.__init__(self, root)
 
         l = tk.Label(self, anchor="c", width=10, height=5)
-        b = tk.Button(self, text="Start Timer", width=50, height=25, bg="red", fg="white", command=self.countdownTimer)
+        b = tk.Button(self, text="Start Timer", width=50, height=25, 
+                      bg="red", fg="white", 
+                      command=self.countdownTimer)
+        
+        c = ChecklistBox(self, ["Fix bugs", "Review PR", "Implement methods"])
+        c.pack()
+
         l.pack(side="top", fill="both", expand=True)
-        t = tk.Text().pack()
         b.pack(side="top", fill="both", expand=True)
 
         self.min = tk.StringVar()
@@ -40,8 +70,8 @@ class Pomodoro(tk.Frame):
 
         self.min.set('25')
         self.sec.set('00')
-        tk.Entry(self, textvariable=self.min, width=2, font = 'Helvetica 14').place(x=250, y=35)
-        tk.Entry(self, textvariable=self.sec, width=2, font = 'Helvetica 14').place(x=275, y=35)
+        tk.Entry(self, textvariable=self.min, width=2, font = 'Helvetica 14').place(x=150, y=90)
+        tk.Entry(self, textvariable=self.sec, width=2, font = 'Helvetica 14').place(x=175, y=90)
 
 if __name__=='__main__':
     root = tk.Tk()
